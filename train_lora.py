373a55eb5c7c782b6022e3fc7ba8e3a2faa22ac8
necrophagists <120618287+necrophagists@users.noreply.github.com>
2023-08-10 10:11:24 +0800

    Add files via upload

--- train_lora.py
+++ train_lora.py
@@ -7,58 +7,75 @@ from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from data_preprocess import load_json_data,get_dataloader
 from steprunner import StepRunner
-from Config import Config
-
-conf =Config()
-train_json_path ="xxx"
-test_json_file ="xxx"
-llm_file_path ="yyy"
+import argparse
 
 
-##data_preprocess
-train_dataset =load_json_data(train_json_path)
-test_dataset =load_json_data(test_json_file)
-train_loader,test_loader =get_dataloader(train_dataset,test_dataset)
+def main():
+    parser =argparse.ArgumentParser()
+    parser.add_argument("--train_data_path",default=None,type=str,required=True)
+    parser.add_argument("--test_data_path",default=None,type=str,required=False)
+    parser.add_argument("--base_llm_path",default=None,type=str,required=True)
+    parser.add_argument("--output_dir",default=None,type=str,required=True)
 
-print("finish data preprocess")
-##config model
-config =AutoConfig.from_pretrained(llm_file_path,trust_remote_code=True)
-model =AutoModel.from_pretrained(llm_file_path,trust_remote_code=True,config=config)
+    parser.add_argument("--stf_mode",default=None,type=str,required=True)
+    parser.add_argument("--lora_r",default=4,type=int,required=False)
+    parser.add_argument("--lora_alpha", default=32, type=int, required=False)
+    parser.add_argument("--lora_drouout", default=0.05, type=float, required=False)
 
-model.supports_gradient_checkpointing =True #开启gradient_ckpt 节约现存
-model.gradient_checkpointing_enable()
-model.enable_input_require_grads()
-
-model.config.use_cache =False #训练不用kv缓存 推理可以用
-
-lora_config =LoraConfig(task_type=TaskType.CAUSAL_LM,inference_mode=False,r=4,lora_alpha=32,lora_dropout=0.05)
-
-model =model.half().cuda()
-model =get_peft_model(model,lora_config)
-model.print_trainable_parameters()    #显示可训练参数的量
-print("finish config lora")
-
-##config training
-#rewrite StepRunner/save_ckpt/load_ckpt
-KerasModel.StepRunner =StepRunner
-KerasModel.save_ckpt =StepRunner.save_ckpt
-KerasModel.load_ckpt =StepRunner.load_ckpt
-
-lr_scheduler =CosineAnnealingLR(torch.optim.AdamW(model.parameters(),lr=conf.lr),T_max=10)
-
-keras_model =KerasModel(model,loss_fn=None,optimizer=torch.optim.AdamW(model.parameters(),lr=conf.lr),
-                        lr_scheduler=lr_scheduler)
-
-ckpt_path ="zzz"
-print("finish config keras model")
-keras_model.fit(train_data=train_loader,
-                val_data=test_loader,
-                epochs=conf.epochs,
-                monitor='train_loss',
-                mode='mean',
-                ckpt_path=ckpt_path,
-                mixed_precision='fp16')
-print("finish lora training!!!")
+    parser.add_argument("--lr", default=5e-4, type=float, required=False)
+    parser.add_argument("--batch_size", default=2, type=float, required=False)
+    parser.add_argument("--max_seq_len",default=2048,type=int,required=False)
 
 
+    args =parser.parse_args()
+    print(args)
+
+
+    ##data_preprocess
+    train_dataset =load_json_data(args.train_data_path)
+    test_dataset =load_json_data(args.train_data_path)
+    train_loader,test_loader =get_dataloader(train_dataset,test_dataset,args.base_llm_path,args.batch_size,args.max_seq_len)
+
+    print("finish data preprocess")
+    ##config model
+    config =AutoConfig.from_pretrained(args.base_llm_path,trust_remote_code=True)
+    model =AutoModel.from_pretrained(args.base_llm_path,trust_remote_code=True,config=config)
+
+    model.supports_gradient_checkpointing =True #开启gradient_ckpt 节约现存
+    model.gradient_checkpointing_enable()
+    model.enable_input_require_grads()
+
+    model.config.use_cache =False #训练不用kv缓存 推理可以用
+
+    lora_config =LoraConfig(task_type=TaskType.CAUSAL_LM,inference_mode=False,r=args.lora_r,
+                            lora_alpha=args.lora_alpha,lora_dropout=args.lora_dropout)
+
+    model =model.half().cuda()
+    model =get_peft_model(model,lora_config)
+    model.print_trainable_parameters()    #显示可训练参数的量
+    print("finish config lora")
+
+    ##config training
+    #rewrite StepRunner/save_ckpt/load_ckpt
+    KerasModel.StepRunner =StepRunner
+    KerasModel.save_ckpt =StepRunner.save_ckpt
+    KerasModel.load_ckpt =StepRunner.load_ckpt
+
+    lr_scheduler =CosineAnnealingLR(torch.optim.AdamW(model.parameters(),lr=args.lr),T_max=10)
+
+    keras_model =KerasModel(model,loss_fn=None,optimizer=torch.optim.AdamW(model.parameters(),lr=args.lr),
+                            lr_scheduler=lr_scheduler)
+
+    print("finish config keras model")
+    keras_model.fit(train_data=train_loader,
+                    val_data=test_loader,
+                    epochs=conf.epochs,
+                    monitor='train_loss',
+                    mode='mean',
+                    ckpt_path=args.output_dir,
+                    mixed_precision='fp16')
+    print("finish lora training!!!")
+
+if __name__ =="__main__":
+    main()
 
